d446f20292f9d44043376dfda8bb9e2769e482d3
Tarun raj naik <74200794+tarun730@users.noreply.github.com>
2024-08-19 22:06:06 +0530

    Update data.js

--- src/assets/data.js
+++ src/assets/data.js
@@ -88,6 +88,10 @@ export const skill = {
                 label: 'MUI',
                 logo: "./tech_stack/material-ui-svgrepo-com.svg"
             },
+             {
+                label: 'POSTMAN',
+                logo: "https://res.cloudinary.com/postman/image/upload/t_team_logo/v1629869194/team/2893aede23f01bfcbd2319326bc96a6ed0524eba759745ed6d73405a3a8b67a8"
+            },
         ]
 
 
